63d854f4b806cfa8001258327d1475a6b5cd79b2
sebhoof <hoof@uni-goettingen.de>
2020-09-23 12:49:44 +0200

    Updated constants, following the recommended value for the solar radius.

--- include/solaxflux/constants.hpp
+++ include/solaxflux/constants.hpp
@@ -5,21 +5,25 @@
 
 #include <gsl/gsl_math.h>
 
-const std::string LIBRARY_NAME = "SolarAxionFlux v0.4b";
+// Library names
+const std::string LIBRARY_NAME = "SolarAxionFlux v0.5b";
 
+// Mathematical constants
 const double pi = M_PI;
+
+// Physical constants
 const double hbar = 6.582119514e-25;                          // in GeV * s
-const double radius_sol = 6.9598e8;                           // in metres
-const double distance_sol = 1.495978707e11;                   // in metres
+const double radius_sol = 6.957e8; // [m]; nominal value of the Solar radius, as recommend by https://www.iau.org/static/resolutions/IAU2015_English.pdf
+const double distance_sol = 1.49597870700e11; // [m]; taken to be 1 au (with the value of 1 au as recommnded by https://www.iau.org/static/resolutions/IAU2012_English.pdf)
 const double alpha_EM = 7.2973525664e-3;                      // fine structure constant
-const double atomic_mass_unit = 0.931494028;                  // atomic mass unit (GeV/c^2)
-const double gev2cm = 197.327053e-16;                         // cm per GeV^-1
-const double eV2g = 1.782661907e-33;                          // eV per g
-const double s2cm = 2.99792458e10;                            // cm per s
-const double keV2cm = 197.327053e-10;                         // cm per keV^-1
+const double atomic_mass_unit = 0.931494028;                  // [GeV/c^2]
+const double gev2cm = 197.327053e-16;                         // cm/GeV^-1
+const double eV2g = 1.782661907e-33;                          // eV/g
+const double s2cm = 2.99792458e10;                            // cm/s
+const double keV2cm = 197.327053e-10;                         // cm/keV^-1
 const double eVm = gev2cm*1.0e7;
 const double K2eV = 8.6173303e-5;                             // eV per K
-const double m_electron = 0.5109989461E+3;                    // electron mass (keV/c^2)
+const double m_electron = 0.5109989461e3;                    // electron mass (keV/c^2)
 const double a_Bohr = 5.29177210903e-9;                       // Bohr radius in cm
 const double g_aee = 1.0E-13;                                 // axion-electron coupling
 const double g_agg = 1.0E-16;                                 // axion-photon coupling in keV^-1
