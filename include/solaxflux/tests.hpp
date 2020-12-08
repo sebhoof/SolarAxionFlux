fda4ae53c83f24b4b88688984537e714b78401b4
Sebastian Hoof <hoof@uni-goettingen.de>
2020-12-08 10:57:24 +0100

    Fully generalised Primakoff rate. Removed comments.

--- include/solaxflux/tests.hpp
+++ include/solaxflux/tests.hpp
@@ -102,47 +102,6 @@ void run_unit_test() {
   auto t4e = std::chrono::high_resolution_clock::now();
   std::cout << "# Calculating the full axion-electron spectrum (" << n_erg_values << " energy values) took " << std::chrono::duration_cast<std::chrono::seconds>(t4e-t4s).count() << " seconds." << std::endl;
 
-  // BEGIN EXPERIMENTAL
-  //std::cout << "\n# Generating MC object from Primakoff spectrum file, drawing 10 random axions energies, and saving inverse cdf data..." << std::endl;
-  //AxionMCGenerator1D mc_engine ("results/primakoff.dat");
-  //std::vector<double> mc_energies = mc_engine.draw_axion_energies(10);
-  //std::cout << "MC'ed axion energies (in keV):" << std::endl;
-  //std::cout << "[ "; for (auto erg = mc_energies.begin(); erg != mc_energies.end(); ++erg) { std::cout << *erg << " "; }; std::cout << "]" << std::endl;
-  //mc_engine.save_inv_cdf_to_file("results/primakoff_inv_cdf.dat");
-  // END EXPERIMENTAL
-  //std::cout << "\n# Computing counts in CAST2007 experiment from axion-photon interactions (full calculation)..." << std::endl;
-  //axion_photon_counts_full(1.0e-3, 1.0e-10, &cast_2007_setup, &s);
-  //auto t6 = std::chrono::high_resolution_clock::now();
-  //std::cout << "# Calculating the counts took " << std::chrono::duration_cast<std::chrono::minutes>(t6-t5).count() << " minutes." << std::endl;
-  //double lowerg = 0.1;
-  //double higherg = 10.0;
-  //std::cout << "# Calculating full flux between " << lowerg << " keV and " << higherg << " keV." << std::endl;
-  //std::cout << calculate_flux(lowerg,higherg,s,{}) << std::endl;
-
-  // BEGIN EXPERIMENTAL (TO REVIEW!)
-  // std::cout << "\n# Computing reference counts in CAST2007 experiment from all interactions (from files)..." << std::endl;
-  // auto t5s = std::chrono::high_resolution_clock::now();
-  // std::vector<double> axion_masses = { 1.0e-6, 2.0e-6, 5.0e-6, 1.0e-5, 2.0e-5, 5.0e-5, 1.0e-4, 2.0e-4, 5.0e-4, 1.0e-3, 2.0e-3, 5.0e-3, 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0 };
-  // axion_reference_counts_from_file(&cast_2007_setup, axion_masses, "results/primakoff.dat", "results/all_gaee.dat", "results/reference_counts_cast2007.dat");
-  // auto t5e = std::chrono::high_resolution_clock::now();
-  // std::cout << "# Calculating " << 2*axion_masses.size()*cast_2007_setup.n_bins << " reference counts took " << std::chrono::duration_cast<std::chrono::seconds>(t5e-t5s).count() << " seconds." << std::endl;
-  // //std::cout << "# Compute counts in CAST2007 experiment from axion-electron interactions..." << std::endl;
-  // //axion_electron_counts_full(1.0e-3, 1.0e-13, 1.0e-10, &setup, &s);
-  // //auto t8 = std::chrono::high_resolution_clock::now();
-  // //std::cout << "# Calculating the counts took " << std::chrono::duration_cast<std::chrono::minutes>(t8-t7).count() << " minutes." << std::endl;
-  // //std::cout << "\n# Compute counts in CAST2007 experiment from axion-electron interactions (from spectrum file)..." << std::endl;
-  // //axion_electron_counts(1.0e-3, 1.0e-13, 1.0e-10, &cast_2007_setup, "results/all_gaee.dat");
-  // std::cout << "\n# Computing predicted counts in CAST2007 experiment (m_a = 0.004 eV, g_agamma = 10^-10/GeV, g_ae = 0 or 10^-12)..." << std::endl;
-  // std::vector<double> counts = counts_prediciton_from_file(4.0e-3, 1.0e-10, "results/reference_counts_cast2007.dat", 0);
-  // //auto t7 = std::chrono::high_resolution_clock::now();
-  // std::cout << "Counts (g_agamma): " << counts[0]; for (auto c = counts.begin()+1; c != counts.end(); ++c) { std::cout << " | " << *c; }; std::cout << std::endl;
-  // //std::cout << "# Initial setup and one calculation took " << std::chrono::duration_cast<std::chrono::seconds>(t7-t6).count() << " seconds." << std::endl;
-  // counts = counts_prediciton_from_file(4.0e-3, 1.0e-10, "results/reference_counts_cast2007.dat", 1.0e-12);
-  // //auto t8 = std::chrono::high_resolution_clock::now();
-  // std::cout << "Counts (all): " << counts[0]; for (auto c = counts.begin()+1; c != counts.end(); ++c) { std::cout << " | " << *c; }; std::cout << std::endl;
-  // //std::cout << "# Setup step does not have to be repeated; one additional calculation took " << std::chrono::duration_cast<std::chrono::seconds>(t8-t7).count() << " seconds." << std::endl;
-  // END EXPERIMENTAL (TO REVIEW!)
-
   auto t_end = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Finished testing! Total runtime: " << std::chrono::duration_cast<std::chrono::minutes>(t_end-t_start).count() << " mins." << std::endl;
 }
