cdc469c08a07fd87215359e44ebc08aa151d8948
Lennert Thormaehlen <l.thormaehlen@icloud.com>
2021-01-06 16:17:11 +0100

    circumvented bug in Gamma_TP_Rosseland and Gamma_LP_Rosseland

--- src/solar_model.cpp
+++ src/solar_model.cpp
@@ -687,6 +687,7 @@ double SolarModel::Gamma_P_LP_Rosseland(double omega, double r) {
   if (omega_pl_squared(r) > omega*omega) { return 0; }  //energy can't be lower than plasma frequency
   double u = omega/temperature_in_keV(r);
   double gamma = -gsl_expm1(-u)*interpolate_rosseland_opacity(r);
+  if ((isnan(gamma) == true) or (isinf(gamma)==true)) {return 0;}
   double average_b_field_sq = gsl_pow_2(bfield(r))/(3.0);
   double DeltaLsq = g_agg*g_agg * average_b_field_sq ;
   const double geom_factor = 1.0;  // factor accounting for observers position (1.0 = angular average)
@@ -724,6 +725,7 @@ double SolarModel::Gamma_P_TP_Rosseland(double omega, double r) {
   if (omega_pl_squared(r) > omega*omega) { return 0; }  //energy can't be lower than plasma frequency
   double u = omega/temperature_in_keV(r);
   double gamma = -gsl_expm1(-u)*interpolate_rosseland_opacity(r);
+  if ((isnan(gamma) == true) or (isinf(gamma)==true)) {return 0;}
   double DeltaPsq = omega*omega * gsl_pow_2(sqrt(1-omega_pl_squared(r)/(omega*omega))-1);   //transfered momentum squared
   double average_b_field_sq = gsl_pow_2(bfield(r))/(3.0);
   double DeltaTsq = g_agg*g_agg * average_b_field_sq /4.0;
