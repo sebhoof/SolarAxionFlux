d24afd12a6b86956171952937a40014d3bbcc79e
Lennert Thormaehlen <l.thormaehlen@icloud.com>
2021-07-01 12:41:17 +0200

    corrected all geometric factors etc for LP flux

--- results/plot.py
+++ results/plot.py
@@ -107,18 +107,18 @@ plot_setup()
 plt.plot(omega, 6.02*omega**2.481*np.exp(-omega/1.205),':', color=col_agss09, label=r'Primakoff approx. (BP04)')
 plt.plot(ref1[:,0], conv_fac*(1.0e4/50.0)*ref1[:,1], '-', color=col_b16agss09, label=r'Primakoff (Redondo)')
 plt.plot(res1[:,0], res1[:,1]/1.0e10, 'k--', label=r'Primakoff (AGSS09)')
-plt.plot(res7[:,0], res7[:,1]/1.0e10*2.0, 'k-', label=r'LP (AGSS09)')
-plt.plot(res9[:,0], res9[:,1]/1.0e10*2.0, 'k--', label=r'LP Rosseland (AGSS09)')
+plt.plot(res7[:,0], res7[:,1]/1.0e10, 'k-', label=r'LP (AGSS09)')
+plt.plot(res9[:,0], res9[:,1]/1.0e10, 'k--', label=r'LP Rosseland (AGSS09)')
 plt.plot(ref6[:,0], ref6[:,1]*4.0, '--', color='green', label=r'LP (Giannotti)') # correct coupling
-plt.plot(ref7[:,0], ref7[:,1]/1.0e10*4.0/1.8, '--', color='orange', label=r'LP (O´Hare)') # correct coupling and angular average
+plt.plot(ref7[:,0], ref7[:,1]/1.0e10*4.0/1.7856, '--', color='orange', label=r'LP (O´Hare)') # correct coupling and angular average
 plt.plot(ref8[:,0], ref8[:,1]/1.0e10*(3.0/5.0)**2, '--', color='gold', label=r'LP (Caputo)') #correct  field values
 
 plt.title(r'Axion-photon interactions, $g_{a\gamma\gamma} = \SI{e-10}{\GeV^{-1}}$, OP opacities')
 plt.xlabel(r'Energy $\omega$ [keV]')
 plt.ylabel(r'Axion flux $\mathrm{d}\Phi_a/\mathrm{d}\omega$ [\SI{e10}{\per\cm\squared\per\keV\per\s}]')
 plt.xlim([0.001,0.4])
-#plt.yscale('log')
-#plt.xscale('log')
+plt.yscale('log')
+plt.xscale('log')
 plt.ylim([0.0,37])
 
 plt.legend(frameon=False)
