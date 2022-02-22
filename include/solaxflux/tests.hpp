2a01e8aff0d9aa15e2bf479d41cf58a937f89c25
Lennert Thormaehlen <l.thormaehlen@icloud.com>
2022-02-22 11:10:18 +0100

    cleaned and shortened test

--- include/solaxflux/tests.hpp
+++ include/solaxflux/tests.hpp
@@ -20,23 +20,26 @@ void run_unit_test() {
   std::cout << "# Testing the Solar Model routines (this may take 10 mins or longer)..." << std::endl;
 
   auto t1s = std::chrono::high_resolution_clock::now();
-  std::string solar_model_name_1 = SOLAXFLUX_DIR "/data/solar_models/SolarModel_B16-AGSS09.dat";
-  std::string solar_model_name_2 = SOLAXFLUX_DIR "/data/solar_models/SolarModel_B16-GS98.dat";
+  std::string solar_model_name = SOLAXFLUX_DIR "/data/solar_models/SolarModel_B16-AGSS09.dat";
   std::string output_path = SOLAXFLUX_DIR "/results/";
   SolarModel s;
-  SolarModel s2;
-  s = SolarModel(solar_model_name_1, OP, true);
-  s2 = SolarModel(solar_model_name_2, OP, true);
-  
+  try {
+    s = SolarModel("WRONG_FILENAME");
+  } catch(XFileNotFound& err) {
+    std::cout << "# Oops, the wrong filename was used... This will throw an error like the one below:" << std::endl;
+    std::cout << err.what() << std::endl;
+    std::cout << "# The error was caught and handled by using the correct filename. The test will continue now..." << std::endl;
+    s = SolarModel(solar_model_name, OP, true);
+  }
   auto t1e = std::chrono::high_resolution_clock::now();
-  std::cout << "\n# Setting up the Solar models '" << solar_model_name_1 <<" and " <<  solar_model_name_2 << " took " << std::chrono::duration_cast<std::chrono::seconds>(t1e-t1s).count() << " seconds." << std::endl;
-  const int n_erg_values = 100;
+  std::cout << "\n# Setting up the Solar model '" << solar_model_name << "' took " << std::chrono::duration_cast<std::chrono::seconds>(t1e-t1s).count() << " seconds." << std::endl;
+  const int n_erg_values = 500;
   const int n_erg_values_LP = 1000;
   const int n_erg_values_Fe57 = 1000;
   std:: vector<double> test_ergs;
-  for (int k=0; k<n_erg_values; k++) { test_ergs.push_back(1.0+k*10.0/n_erg_values); }
+  for (int k=0; k<n_erg_values; k++) { test_ergs.push_back(0.1+k*11.9/n_erg_values); }
   std:: vector<double> test_ergs_LP;
-  for (int k=0; k<n_erg_values_LP; k++) { test_ergs_LP.push_back(0.001*gsl_pow_int(1.006,k)); }
+  for (int k=0; k<n_erg_values_LP; k++) { test_ergs_LP.push_back((0.001*gsl_pow_int(1.006,k))); }
   std::vector<double> test_ergs_Fe;
   double width = 0.1;
   double step = width / n_erg_values_Fe57;
@@ -52,14 +55,12 @@ void run_unit_test() {
   double res_width = -gsl_expm1(-sqrt(s.omega_pl_squared(0))/ s.temperature_in_keV(0))*s.opacity(sqrt(s.omega_pl_squared(0)),0);
   std::cout << "Width of LP resonance in the core: " << res_width << " keV (should be approx. 0.00189 keV)." << std::endl;
 
-    
-
   auto t2s = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Calculating Primakoff spectrum..." << std::endl;
   calculate_spectral_flux_Primakoff(test_ergs, s, output_path + "primakoff.dat");
   auto t2e = std::chrono::high_resolution_clock::now();
   std::cout << "# Calculating the full Primakoff spectrum (" << n_erg_values << " energy values) took " << std::chrono::duration_cast<std::chrono::milliseconds>(t2e-t2s).count()/1000.0 << " seconds." << std::endl;
-/*
+
   auto t3s = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Calculating non-resonant transversal plasmon spectrum..." << std::endl;
   calculate_spectral_flux(test_ergs, s, &SolarModel::Gamma_P_TP, output_path + "TP.dat");
@@ -95,7 +96,7 @@ void run_unit_test() {
   calculate_total_flux_solar_disc_at_fixed_radii(0.0, 50.0, test_rads, s, &SolarModel::Gamma_P_Primakoff, output_path + "primakoff_integrated_fluxes.dat");
   auto t8e = std::chrono::high_resolution_clock::now();
   std::cout << "# Calculating integrated Primakoff flux for " << n_rad_values << " different radii took " << std::chrono::duration_cast<std::chrono::seconds>(t8e-t8s).count() << " seconds." << std::endl;
-
+/*
   auto t9s = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Calculating Compton spectrum..." << std::endl;
   calculate_spectral_flux_Compton(test_ergs, s, output_path + "compton.dat");
@@ -113,67 +114,25 @@ void run_unit_test() {
   calculate_spectral_flux_opacity(test_ergs, s, output_path + "metals.dat");
   auto t11e = std::chrono::high_resolution_clock::now();
   std::cout << "# Calculating the opacity spectrum (" << n_erg_values << " energy values) took " << std::chrono::duration_cast<std::chrono::seconds>(t11e-t11s).count() << " seconds." << std::endl;
-
+*/
   auto t12s = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Computing full axion-electron spectrum..." << std::endl;
   calculate_spectral_flux_axionelectron(test_ergs, s, output_path + "all_gaee.dat");
   auto t12e = std::chrono::high_resolution_clock::now();
   std::cout << "# Calculating the full axion-electron spectrum (" << n_erg_values << " energy values) took " << std::chrono::duration_cast<std::chrono::seconds>(t12e-t12s).count() << " seconds." << std::endl;
- */
-    for (double rmax = 0.001; rmax < .7; rmax+= 0.7/50.){
-        std::vector<double> spectral = calculate_spectral_flux_solar_disc(test_ergs, rmax, s, &SolarModel::Gamma_P_Primakoff, output_path + "partial_disc_P" +std::to_string(rmax) +".dat" );
-        double flux=0;
-        for(std::vector<double>::iterator it = spectral.begin(); it != spectral.end(); ++it)
-            flux += *it;
-        std::cout << flux << ", ";
-    }
-    std::cout << std::endl;
-    for (double rmax = 0.001; rmax < .7; rmax+= 0.7/50.){
-        std::cout << rmax << ", ";}
-    std::cout << std::endl;
-    
-    for (double rmax = 0.001; rmax < .25; rmax+= 0.25/50.){
-        std::vector<double> spectral = calculate_spectral_flux_solar_disc(test_ergs_Fe, rmax, s, &SolarModel::Gamma_P_Fe57, output_path + "partial_disc" +std::to_string(rmax) +".dat" );
-        double flux=0;
-        for(std::vector<double>::iterator it = spectral.begin(); it != spectral.end(); ++it)
-            flux += *it;
-        std::cout << flux << ", ";
-    }
-    std::cout << std::endl;
-    for (double rmax = 0.001; rmax < .25; rmax+= 0.25/50.){
-        std::cout << rmax << ", ";}
-    std::cout << std::endl;
-    
+   
   auto t13s = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Computing spectrum from Fe57 nuclear transition" << std::endl;
   std::vector<double> specFeFlux = calculate_spectral_flux(test_ergs_Fe, s, &SolarModel::Gamma_P_Fe57, output_path + "Fe57.dat");
-  std::vector<double> specFeFlux2 = calculate_spectral_flux(test_ergs_Fe, s2, &SolarModel::Gamma_P_Fe57, output_path + "Fe57BP2000.dat");
   auto t13e = std::chrono::high_resolution_clock::now();
   std::cout << "# Calculating the spectrum from Fe57 nuclear transition for (" << n_erg_values_Fe57 << " energy values) took " << std::chrono::duration_cast<std::chrono::seconds>(t13e-t13s).count() << " seconds." << std::endl;
   double integratedflux = 0.0;
   for (std::vector<double>::iterator it=specFeFlux.begin() ; it != specFeFlux.end(); ++it) {
         integratedflux += *it;}
   integratedflux *= step;
-  double integratedflux2 = 0.0;
-  for (std::vector<double>::iterator it=specFeFlux2.begin() ; it != specFeFlux2.end(); ++it) {
-           integratedflux2 += *it;
-       }
-  integratedflux2 *= step;
     
-  double lum = 0.0;
-  std::vector<double>::iterator iterg = test_ergs_Fe.begin();
-  for (std::vector<double>::iterator it=specFeFlux.begin() ; it != specFeFlux.end(); ++it) {
-        lum += *it * *iterg ;
-        ++iterg;
-  }
-    lum *= step * 4 * pi * distance_sol * distance_sol * 1.0e4 * 1.602176634e-16;
-  
-  std::cout << "Validation of Fe57 flux:"<< std::endl << solar_model_name_1 << ": Integrated flux = " << integratedflux << " g_eff^2 cm^-2 s^-1" << std::endl << solar_model_name_2 <<": Integrated flux = " << integratedflux2 << " g_eff^2 cm^-2 s^-1" << std::endl << "CAST collaboration found 4.56E+23 g_eff^2 cm^-2 s^-1" << std::endl;
+  std::cout << "Validation of Fe57 flux:"<< std::endl << solar_model_name << ": Integrated flux = " << integratedflux << " g_eff^2 cm^-2 s^-1" << std::endl;
 
-  std::cout << lum << std::endl;
- 
-  std::cout << std::endl << s.temperature_in_keV(0) << "  " << s.density(0) << "  " << s.n_element(0, "Fe") << std::endl;
-  std::cout << std::endl << s2.temperature_in_keV(0) << "  " << s2.density(0) << "  " << s2.n_element(0, "Fe") << std::endl;
   auto t_end = std::chrono::high_resolution_clock::now();
   std::cout << "\n# Finished testing! Total runtime: " << std::chrono::duration_cast<std::chrono::minutes>(t_end-t_start).count() << " mins." << std::endl;
 }
